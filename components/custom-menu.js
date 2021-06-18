9bd530bbd6f56a6e04815a2400963fe62ef2d6a7
Savio Santos <saviosa08@gmail.com>
2021-06-18 07:21:03 -0300

    :recycle: Improved meny layout for mobile apps.

--- components/custom-menu.js
+++ components/custom-menu.js
@@ -1,25 +1,43 @@
 import { Component } from "react";
 
 export default class CustomMenu extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            menu: false
+        };
+        this.toggleMenu = this.toggleMenu.bind(this);
+    }
 
+    toggleMenu() {
+        this.setState({ menu: !this.state.menu })
+    }
+        
     render() {
+        const show = (this.state.menu) ? "show" : "";
+
         return (
             <nav style={{ background: "#164A41" }} className="navbar navbar-expand-lg navbar-light">
-                <div className="container-fluid justify-content-between">
+                <div className="container-fluid">
                     <a className="navbar-brand" href="/">
                         <img width="80" src="logo.png" />
                     </a>
-                    <ul className="nav nav-tabs">
-                        <li className="nav-item">
-                            <a style={{ color: "#F1B24A" }} className="nav-link" href="/criar-tabela">Criar tabela</a>
-                        </li>
-                        <li className="nav-item">
-                            <a style={{ color: "#F1B24A" }} className="nav-link" href="/arquivos">Arquivos</a>
-                        </li>
-                        <li className="nav-item">
-                            <a style={{ color: "#F1B24A" }} className="nav-link" href="/termos">Termos de uso</a>
-                        </li>
-                    </ul>
+                    <button onClick={this.toggleMenu} className="navbar-toggler bg-light" type="button" data-toggle="collapse" data-target="#navbarApp">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className={"collapse navbar-collapse justify-content-end " + show} id="navbarApp">
+                        <ul className={show ? "navbar-nav me-auto": "nav nav-tabs"}>
+                            <li className="nav-item">
+                                <a style={{ color: "#F1B24A" }} className="nav-link" href="/criar-tabela">Criar tabela</a>
+                            </li>
+                            <li className="nav-item">
+                                <a style={{ color: "#F1B24A" }} className="nav-link" href="/arquivos">Arquivos</a>
+                            </li>
+                            <li className="nav-item">
+                                <a style={{ color: "#F1B24A" }} className="nav-link" href="/termos">Termos de uso</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
         )
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css';
+
 import '../styles/globals.css';
 
 export default function SoccerTable({ Component, pageProps }) {
