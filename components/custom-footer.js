3f76d8cdacf3f5a50cd994888326a31c4a4dc641
Sávio Santos <50780673+savi8sant8s@users.noreply.github.com>
2022-03-28 13:53:36 -0300

    Changed footer link.

--- components/custom-footer.js
+++ components/custom-footer.js
@@ -9,7 +9,7 @@ export default class CustomFooter extends Component {
         return (
             <footer className={`footer mt-3 p-3 ${this.props.fixed ? "fixed-bottom" : ""}`}>
                 <div className="container text-center text-light">
-                    Construído por <a style={{ color: "#F1B24A" }} target="_blank" href="https://saviosantos.vercel.app/">Sávio Santos.</a><br></br>
+                    Construído por <a style={{ color: "#F1B24A" }} target="_blank" href="https://github.com/savi8sant8s">Sávio Santos.</a><br></br>
                 Este trabalho está licenciado com uma Licença <a target="_blank" rel="license" href="http://creativecommons.org/licenses/by/4.0/">
                         <img alt="Licença Creative Commons" style={{ borderWidth: "0" }} src="https://i.creativecommons.org/l/by/4.0/88x31.png" />
                     </a>
